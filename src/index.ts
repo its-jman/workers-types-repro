2d14111b8e2867ed36e556990db9da474ff659d8
Josh Manning <josh@jman.me>
2024-04-14 17:29:46 -0700

    5 without rpc

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import {DurableObject, WorkerEntrypoint} from 'cloudflare:workers'
+import {z} from "zod"
 
 export interface Env {
-	MANAGER: DurableObjectNamespace<Manager>
+	MANAGER: DurableObjectNamespace
 }
 
-export class Manager extends DurableObject<Env> {
+export class Manager  {
+	constructor(private ctx: DurableObjectState, private env: Env) {}
+	async fetch(req: Request) {
+		const data = z.object({at: z.number(), val: z.string()}).parse(await req.json())
+		const resp = await this.test(data.at, data.val)
+		return new Response(resp)
+	}
 	async test(at: number, val: string) {
 		await this.ctx.storage.put("test", val)
 		await this.ctx.storage.setAlarm(at)
+		return "ok"
   }
 }
 
